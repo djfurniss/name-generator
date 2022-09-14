e129bc1d91d451a4f73975da3b272905088d37de
Denasia <dfurniss@ymail.com>
2022-09-14 17:57:44 -0400

    added button to generate random names from api

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,28 @@
 import React, { useState, useEffect } from 'react'
 
 const App = () => {
+
   const [name, setName] = useState([])
 
-  useEffect(() => {
-    fetch('https://intense-island-24747.herokuapp.com/')
-      .then((res) => res.json())
-      .then((nme) => setName([nme.message]))
-  }, [])
+  // useEffect(() => {
+  //   fetch('https://intense-island-24747.herokuapp.com/generate')
+  //   .then((res) => res.json())
+  //   .then((nme) => setName([nme]))
+  // }, [])
+  
+  const clickHandler = (event) => {
+    fetch('https://intense-island-24747.herokuapp.com/generate')
+    .then((res) => res.json())
+    .then((nme) => setName([nme]))
+  };
 
   return (
-    <h1>{name}</h1>
+    <div>
+      <h1>Random Name Generator App</h1>
+      <button
+        onClick={clickHandler}>Click to choose a random name</button>
+      <h3>{name}</h3>
+    </div>
   )
 };
 
